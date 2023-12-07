971773e6550ec95d3157445117f0d64736b0b896
popcorn <popcorn@acrossbyte.com>
2023-12-07 14:01:49 +0700

    fx

--- resources/views/screenshot.blade.php
+++ resources/views/screenshot.blade.php
@@ -2,22 +2,12 @@
 
 <html>
 <head>
-    <title>Share</title>
-    <meta property="og:url" content="{{ $facebook }}" />
-        <meta property="og:type" content="article" />
-        <meta property="og:title" content="{{ $title }}" />
-        <meta property="og:description" content="{{ $content }}" />
-        <meta property="og:image" content="{{ $image }}" />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
+    <title>{{$title}}</title>
 </head>
 <body>
-<div id="showShareButton">
-    <img src="{{ $image }}" alt="Screenshot">
-    <h1>{{$title }}</h1>
-    <p>{{ $content }}</p>
-</div>
-
+<img src="{{ $image }}" alt="Screenshot">
+<h1>{{$title }}</h1>
+<p>{{ $content }}</p>
 </body>
 <script>
     window.location.href = "{{ $facebook }}";
