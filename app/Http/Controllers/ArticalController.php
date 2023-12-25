b7a78c3e54aaa23df83db157c48dc2f1eee81d81
limseang <povlimseang.com@gmail.com>
2023-12-25 19:13:52 +0700

    fx

--- app/Http/Controllers/ArticalController.php
+++ app/Http/Controllers/ArticalController.php
@@ -503,7 +503,7 @@ class ArticalController extends Controller
                         'id' => $film->id,
                         'title' => $film->title,
                         'description' => $film->description,
-                        'category' => $film->filmCategories ? $this->getCategoryResource($film->filmCategories) : '',
+                        'category' => $film->filmCategories ? $film->filmCategories->name : '',
                         'poster' => $film->poster ? $uploadController->getSignedUrl($film->poster) : null,
 
                     ];
