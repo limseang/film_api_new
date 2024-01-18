38c84d7c51ab5880e0d4f4f4420f0f4b96bda44f
popcorn <popcorn@acrossbyte.com>
2024-01-18 08:31:49 +0700

    fx

--- app/Http/Controllers/FilmController.php
+++ app/Http/Controllers/FilmController.php
@@ -259,7 +259,6 @@ class FilmController extends Controller
                 'poster' => $film->poster ? $uploadController->getSignedUrl($film->poster) : null,
                 'trailer' => $film->trailer,
                 'type' => $film->types->name ?? $film->type,
-                'director' => $film->directors->name ?? $film->director,
                 'running_time' => $film->running_time,
                 'language' => $film->languages->language ?? $film->language,
                 'rating' => (string) $this->countRate($film->id),
