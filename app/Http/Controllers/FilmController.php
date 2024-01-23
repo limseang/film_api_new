b8c6f7d6f758be39e2951e37b7e55d1b10a8c6a5
popcorn <popcorn@acrossbyte.com>
2024-01-23 11:55:59 +0700

    fx

--- app/Http/Controllers/FilmController.php
+++ app/Http/Controllers/FilmController.php
@@ -105,7 +105,7 @@ class FilmController extends Controller
                 'season' => $item->season,
                 'release_date' => $item->release_date,
                 'file' => $item->file,
-                'poster' => $uploadController->getSignedUrl($item->poster),
+                'poster' => $item->poster ? $uploadController->getSignedUrl($item->poster) : $film_id->poster,
             ];
         }
         return $filmEpisode;
