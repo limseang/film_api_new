34c6d32134c44d9b75310a6f89a53fca3164eb2e
popcorn <popcorn@acrossbyte.com>
2024-01-24 15:40:30 +0700

    fx

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -205,11 +205,13 @@ class UserController extends Controller
          else{
              $user['avatar'] = 'https://cinemagickh.oss-ap-southeast-7.aliyuncs.com/uploads/2023/05/31/220e277427af033f682f8709e54711ab.webp';
          }
+         $users = $user->toArray();
          $response =[];
-         foreach ($user as $value){
-             $response = (string)$value;
+         if(!empty($users)){
+             foreach ($users as  $key=> $value){
+                 $response[$key] = (string)$value;
+             }
          }
-
 
 
          return response()->json([
