e2e40259e5c34247b071b97ac1ceec2994a63756
popcorn <popcorn@acrossbyte.com>
2024-03-23 15:38:12 +0700

    fx

--- app/Http/Controllers/ArticalController.php
+++ app/Http/Controllers/ArticalController.php
@@ -455,7 +455,7 @@ class ArticalController extends Controller
             $video = video::with(['film', 'article', 'categories','types','tags']);
             $tag = Tag::all();
             if($request->title){
-                if($request->most_watch){
+                if($request->most_watch == 'true'){
                     $film->where('title', 'like', '%' . $request->title . '%')->orWhereHas('tags',function ($query) use ($request) {$query->where('name', 'like', '%' . $request->title . '%');});
                 }
                 $artical->where('title', 'like', '%' . $request->title . '%');
