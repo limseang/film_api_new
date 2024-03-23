593268a7a10d674d5d6ab092cfe12cde4f14905c
popcorn <popcorn@acrossbyte.com>
2024-03-23 15:47:48 +0700

    fx

--- app/Http/Controllers/ArticalController.php
+++ app/Http/Controllers/ArticalController.php
@@ -458,10 +458,16 @@ class ArticalController extends Controller
                 if($request->most_watch == 'true'){
                     $film->where('title', 'like', '%' . $request->title . '%')->orWhereHas('tags',function ($query) use ($request) {$query->where('name', 'like', '%' . $request->title . '%');});
                 }
-                $artical->where('title', 'like', '%' . $request->title . '%');
-                $film->where('title', 'like', '%' . $request->title . '%')->orWhereHas('tags',function ($query) use ($request) {$query->where('name', 'like', '%' . $request->title . '%');});
-                $video->where('title', 'like', '%' . $request->title . '%', 'or', 'tags', 'like', '%' . $request->title . '%');
-                $tag->where('name', 'like', '%' . $request->title . '%');
+                else {
+                    $artical->where('title', 'like', '%' . $request->title . '%');
+                    $film->where('title', 'like', '%' . $request->title . '%')->orWhereHas('tags',function ($query) use ($request) {$query->where('name', 'like', '%' . $request->title . '%');});
+                    $video->where('title', 'like', '%' . $request->title . '%', 'or', 'tags', 'like', '%' . $request->title . '%');
+                    $tag->where('name', 'like', '%' . $request->title . '%');
+                }
+//                $artical->where('title', 'like', '%' . $request->title . '%');
+//                $film->where('title', 'like', '%' . $request->title . '%')->orWhereHas('tags',function ($query) use ($request) {$query->where('name', 'like', '%' . $request->title . '%');});
+//                $video->where('title', 'like', '%' . $request->title . '%', 'or', 'tags', 'like', '%' . $request->title . '%');
+//                $tag->where('name', 'like', '%' . $request->title . '%');
 //                $film->whereHas('tags', function ($query) use ($request) {$query->where('name', 'like', '%' . $request->title . '%');});
 
             }
