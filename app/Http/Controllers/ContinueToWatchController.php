06a109d0ec557503d233f4a3ccda757a524cd55c
limseang <povliseang.com@gmail.com>
2024-05-26 15:48:33 +0700

    fx

--- app/Http/Controllers/ContinueToWatchController.php
+++ app/Http/Controllers/ContinueToWatchController.php
@@ -91,7 +91,7 @@ class ContinueToWatchController extends Controller
                     'user_id' => $item->user_id,
                     'films' => $item->films->title ?? '',
                     'film_id' => $item->film_id,
-                    'poster' => $item->films->poster ? $uploadController->getSignedUrl($item->films->poster) : '',
+                    'poster' => $uploadController->getSignedUrl($item->films->poster),
                     'episodes' => $item->episodes->episode ?? '',
                     'progressing' => $item->progressing,
                     'episode_id' => $item->episode_id,
