61865edb66ebde1ebc1ad58c4e824fafe9c553d6
limseang <povliseang.com@gmail.com>
2024-06-02 00:56:17 +0700

    fx

--- routes/api.php
+++ routes/api.php
@@ -113,7 +113,7 @@ Route::group(['middleware' => ['auth:sanctum']], function (){
 /* User Premium */
 Route::group(['middleware' => ['auth:sanctum']], function (){
     Route::post('/user/premium/create/', [PremiumUserController::class, 'create']);
-    Route::get('/user/premium', [PremiumUserController::class, 'ownPremium']);
+    Route::get('/premium', [PremiumUserController::class, 'ownPremium']);
     Route::group(['middleware' => ['postpermission']], function () {
         Route::get('/all/user/premium', [PremiumUserController::class, 'index']);
         Route::delete('/user/premium/delete/{id}', [PremiumUserController::class, 'destroy']);
