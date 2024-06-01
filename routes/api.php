ffe648780b1faa58e6a1fca17428d15f19c82a16
limseang <povliseang.com@gmail.com>
2024-06-02 00:59:35 +0700

    fx

--- routes/api.php
+++ routes/api.php
@@ -111,17 +111,7 @@ Route::group(['middleware' => ['auth:sanctum']], function (){
 });
 
 /* User Premium */
-Route::group(['middleware' => ['auth:sanctum']], function (){
-    Route::post('/user/premium/create/', [PremiumUserController::class, 'create']);
-    Route::get('/premium', [PremiumUserController::class, 'ownPremium']);
-    Route::group(['middleware' => ['postpermission']], function () {
-        Route::get('/all/user/premium', [PremiumUserController::class, 'index']);
-        Route::delete('/user/premium/delete/{id}', [PremiumUserController::class, 'destroy']);
 
-        Route::post('/user/premium/change/status/', [PremiumUserController::class, 'changeStatus']);
-    });
-
-});
 
 /* UserType */
 
@@ -606,6 +596,17 @@ Route::group(['middleware' => ['auth:sanctum']], function (){
     });
 });
 
+
+Route::group(['middleware' => ['auth:sanctum']], function (){
+    Route::post('/user/premium/create/', [PremiumUserController::class, 'create']);
+    Route::get('/premium', [PremiumUserController::class, 'ownPremium']);
+    Route::group(['middleware' => ['postpermission']], function () {
+        Route::get('/all/user/premium', [PremiumUserController::class, 'index']);
+        Route::delete('/user/premium/delete/{id}', [PremiumUserController::class, 'destroy']);
+        Route::post('/user/premium/change/status/', [PremiumUserController::class, 'changeStatus']);
+    });
+
+});
 
 
 
