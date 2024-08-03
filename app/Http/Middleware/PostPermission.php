a5a800108ddc986ddafac61969b00fbe083565a4
limseang <povlimseang.com@gmail.com>
2024-08-03 22:44:13 +0700

    fx

--- app/Http/Middleware/PostPermission.php
+++ app/Http/Middleware/PostPermission.php
@@ -16,8 +16,9 @@ class PostPermission
     public function handle(Request $request, Closure $next): Response
     {
         if(
-            auth()->user()->role_id == 1 ||
-            auth()->user()->role_id == 2 ||
+            auth()->user()->role_id == "1" ||
+            auth()->user()->role_id == "2" ||
+
             auth()->user()->id == $request->id
         ){
             return $next($request);
