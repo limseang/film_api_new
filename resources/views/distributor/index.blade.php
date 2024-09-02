5fcf362797560954d012e10dfc5120fe8b4b1c58
LAPTOP-DG1C2FKI\ASUS <phallaleang731@gmail.com>
2024-09-02 14:10:16 +0700

    Update button add

--- resources/views/distributor/index.blade.php
+++ resources/views/distributor/index.blade.php
@@ -3,12 +3,9 @@
 {{__('sma.distributor')}}
 @endsection
 @section('breadcrumb-topbar')
-<span data-toggle="tooltip" role="button" type="button" aria-haspopup="true" title="{{__('global.add_new')}}"> 
-    <a href="{{route('distributor.create')}}" type="button" class="{{config('setup.button_add')}}">
-     <i class="ph-plus"></i>
+  <a href="{{route('distributor.create')}}" data-toggle="tooltip" role="button" type="button" aria-haspopup="true" class="d-flex align-items-center text-body">
+    <span class="{{config('setup.button_add')}}"> <i class="ph-plus"></i> </span> &nbsp; <span> {{__('global.add_new')}} </span>
   </a>
-  </span>
-
 @endsection
 @section('content')
     <!-- Search field -->
--- resources/views/genre/index.blade.php
+++ resources/views/genre/index.blade.php
@@ -3,12 +3,9 @@
 {{__('sma.genre')}}
 @endsection
 @section('breadcrumb-topbar')
-<span data-toggle="tooltip" role="button" type="button" aria-haspopup="true" title="{{__('global.add_new')}}"> 
-    <a href="{{route('genre.create')}}" type="button" class="{{config('setup.button_add')}}">
-     <i class="ph-plus"></i>
+  <a href="{{route('genre.create')}}" data-toggle="tooltip" role="button" type="button" aria-haspopup="true" class="d-flex align-items-center text-body">
+    <span class="{{config('setup.button_add')}}"> <i class="ph-plus"></i> </span> &nbsp; <span> {{__('global.add_new')}} </span>
   </a>
-  </span>
-
 @endsection
 @section('content')
     <!-- Search field -->
