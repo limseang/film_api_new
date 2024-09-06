01d2d49f2bb9c759b003bdc3f2adf6506829bda5
limseang <povlimseang.com@gmail.com>
2024-09-06 21:28:25 +0700

    x

--- app/Http/Controllers/SubcriptController.php
+++ app/Http/Controllers/SubcriptController.php
@@ -132,7 +132,7 @@ class SubcriptController extends Controller
      */
     function generateAppleJWT()
     {
-        $privateKey = env('APPLE_PRIVATE_KEY'); // Fetch from .env
+        $privateKey = file_get_contents(storage_path('storage/app/AuthKey_Y86Q74HSM8.p8'));
         $keyId = 'Y86Q74HSM8'; // Your Apple Key ID
         $issuerId = 'VZU47BRDUA'; // Your Apple Developer Team ID
 
