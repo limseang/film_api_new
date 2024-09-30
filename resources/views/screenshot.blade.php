3dc345bbbc468f0aec966cf8c69e687685bb66b0
lim seang <povlimseang.com@gmail.com>
2024-10-01 03:14:45 +0700

    fx

--- resources/views/screenshot.blade.php
+++ resources/views/screenshot.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <!-- Open Graph Meta Tags -->
+    <!-- Required Open Graph Meta Tags for Facebook -->
     <meta property="og:type" content="website">
     <meta property="og:title" content="{{ $title }}">
     <meta property="og:description" content="{{ $content }}">
@@ -14,8 +14,9 @@
     <meta property="og:image:height" content="630">
     <meta property="og:url" content="{{ url()->current() }}">
     <meta property="og:site_name" content="CineMagicKH">
+    <meta property="fb:app_id" content="834525820555880"> <!-- Add this line with your app ID -->
 
-    <!-- Twitter Card Meta Tags -->
+    <!-- Additional Twitter Card Meta Tags -->
     <meta name="twitter:card" content="summary_large_image">
     <meta name="twitter:title" content="{{ $title }}">
     <meta name="twitter:description" content="{{ $content }}">
@@ -25,6 +26,7 @@
     <!-- Canonical URL -->
     <link rel="canonical" href="{{ url()->current() }}">
 
+    <!-- Page Title -->
     <title>{{ $title }}</title>
 </head>
 <body>
