bc5706a1fda5e7b8c120a9afc67e57dea96697e0
lim seang <povlimseang.com@gmail.com>
2024-10-15 14:30:43 +0700

    fx

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -606,11 +606,11 @@ class UserController extends Controller
         $data = $request->all();
 
         // Log incoming data for debugging
-        Log::info('Telegram Login Data:', $data);
+//        Log::info('Telegram Login Data:', $data);
 
         // Ensure all required parameters are present
         log::error('hash array', $data['hash']);
-        log::error('id array', $data->id);
+//        log::error('id array', $data->id);
         if (!isset($data->hash, $data->id, $data->auth_date)) {
             Log::error('Missing required parameters.', $data);
             return response()->json(['error' => 'Missing required parameters'], 400);
