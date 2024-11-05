afde6ffe69fdc64a8dc0490757a064b3592a1b93
lim seang <povlimseang.com@gmail.com>
2024-11-05 22:23:35 +0700

    fx

--- routes/api.php
+++ routes/api.php
@@ -326,6 +326,7 @@ Route::post('/film/search', [FilmController::class, 'searchMovie']);
 Route::post('/film/increase/view/{id}', [FilmController::class, 'IncrementViewCount']);
 Route::group(['middleware' => ['auth:sanctum']], function (){
     Route::group(['middleware' => ['postpermission']], function () {
+        Route::get('/film/detail/{id}', [FilmController::class, 'showByID']);
         Route::post('/film/new', [FilmController::class, 'create']);
         Route::post('/film/type/update/{id}', [FilmController::class, 'typeForMovie']);
         Route::post('/film/update/{id}', [FilmController::class, 'updateFilm']);
